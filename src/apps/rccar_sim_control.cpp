c626841a244d250bbf28eb12f75d9118b2228c5f
Ruixiang Du <ruixiang.du@gmail.com>
2018-07-26 00:04:44 -0400

    major revision, synchronized code from librav repo

---
+++ src/apps/rccar_sim_control.cpp
@@ -0,0 +1,30 @@
+/*
+ * rc_car_sim_demo.cpp
+ *
+ *  Created on: Sep 1, 2016
+ *      Author: rdu
+ */
+
+#include <iostream>
+#include <memory>
+#include <cmath>
+
+#include "rccar_sim/rccar_sim_client.hpp"
+#include "vrep_interface/vrep_sim_process.hpp"
+
+using namespace librav;
+
+int main(int arc, char *argv[])
+{
+	std::shared_ptr<RCCarSimClient> client = std::make_shared<RCCarSimClient>();
+
+	// create a simulation process
+	VrepSimProcess<DataFromRCCarSim, DataToRCCarSim> process(client);
+
+	// run the simulation in synchronous mode
+	if (process.ConnectToServer())
+		// process.StartSimLoop_Synchronous();
+		process.StartSimLoop_ASynchronous();
+
+	return 0;
+}
